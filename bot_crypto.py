29a8095dd2d235f27ea0c636cc24254cb2f5931a
Ilham Mukti <72337230+ilham-mukti@users.noreply.github.com>
2022-07-17 01:12:30 +0700

    fix

--- bot_crypto.py
+++ bot_crypto.py
@@ -10,35 +10,17 @@ class BotCrypto:
         self.params = {
             'quoteAsset': 'USDT',
             'offset': '0',
-            'limit': '100',
-        }
-
-        self.headers = {
-            'authority': 'www.tokocrypto.com',
-            'accept': 'application/json, text/plain, */*',
-            'accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
-            'device': '4',
-            'deviceno': 'b33ad919cf38f746bea88654d039769a',
-            'language': 'id',
-            'referer': 'https://www.tokocrypto.com/markets',
-            'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Linux"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-origin',
-            'user-agent': 'Mozilla/5.0 (Linux NT 10.0; Lin64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
-            'x-trace-id': 'a917dd74-d1b0-4104-9848-69193501c1b0',
+            'limit': '1000',
         }
 
     def request_data(self):
         proxy = {
-            "http": 'http://138.68.238.19/:31028',
-            "http": 'http://170.39.194.16/:3128'
+            "http": 'http://8.219.97.248:80',
+            "https": 'https://8.219.97.248:80'
         }
         s = requests.Session()
         url_data = "https://www.tokocrypto.com/v1/market/trading-pairs"
-        response = s.get(url_data, params=self.params, headers=self.headers, proxies=proxy)
+        response = s.get(url_data, params=self.params, proxies=proxy)
         response = response.json()
         results = response['data']['list']
 
--- ini_tes.py
+++ ini_tes.py
@@ -2,7 +2,7 @@ import requests
 
 proxy = {
     "http": 'http://8.219.97.248:80',
-    "https": 'http://8.219.97.248:80'
+    "https": 'https://8.219.97.248:80'
 }
 url = "https://httpbin.org/ip"
 url_data = "https://www.tokocrypto.com/v1/market/trading-pairs?quoteAsset=USDT&offset=0&limit=10"
