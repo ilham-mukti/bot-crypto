ef347145c88ae7f3eec287bf8a76024be024f1dc
Ilham Mukti <72337230+ilham-mukti@users.noreply.github.com>
2022-07-16 19:05:58 +0700

    Update ini_tes.py

--- ini_tes.py
+++ ini_tes.py
@@ -1,5 +1,5 @@
 import requests
 
-url_data = "https://www.tokocrypto.com/v1/market/trading-pairs?quoteAsset=USDT&offset=0&limit=10"
+url_data = "https://www.binance.info/bapi/composite/v1/public/marketing/symbol/list"
 response = requests.get(url_data).json()
 print(response)
