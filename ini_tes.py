bbd623a12d310dcaacfcce4819cdfdee1dd2653c
Ilham Mukti <72337230+ilham-mukti@users.noreply.github.com>
2022-07-17 00:20:17 +0700

    lagi

--- ini_tes.py
+++ ini_tes.py
@@ -1,10 +1,10 @@
 import requests
-import cloudscraper
 
 proxy = {
-    "http": 'http://103.214.54.254:8080',
+    "http": 'http://185.61.152.137:8080',
 }
-url_data = "https://www.tokocrypto.com/v1/market/trading-pairs?quoteAsset=USDT&offset=0&limit=10"
-scraper = cloudscraper.create_scraper()
-response = scraper.get(url_data)
+url = "https://httpbin.org/ip"
+#url_data = "https://www.tokocrypto.com/v1/market/trading-pairs?quoteAsset=USDT&offset=0&limit=10"
+#scraper = cloudscraper.create_scraper()
+response = requests.get(url, proxies=proxy)
 print(response.json())
