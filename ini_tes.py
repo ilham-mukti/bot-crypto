17b95fb5338a97f13fce3c7c830fdfa99d873808
Ilham Mukti <72337230+ilham-mukti@users.noreply.github.com>
2022-07-16 23:34:45 +0700

    ini bener

--- ini_tes.py
+++ ini_tes.py
@@ -5,5 +5,6 @@ proxy = {
     "http": 'http://103.214.54.254:8080',
 }
 url_data = "https://www.tokocrypto.com/v1/market/trading-pairs?quoteAsset=USDT&offset=0&limit=10"
-response = cloudscraper.create_scraper()
+scraper = cloudscraper.create_scraper()
+response = scraper.get(url_data)
 print(response.json())
