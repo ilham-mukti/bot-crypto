31afc88563ba8dba7a4a1c3d4f2ffd110130ef0f
Ilham Mukti <72337230+ilham-mukti@users.noreply.github.com>
2022-07-16 23:32:42 +0700

    coba lagi

--- ini_tes.py
+++ ini_tes.py
@@ -1,9 +1,9 @@
 import requests
+import cloudscraper
 
 proxy = {
     "http": 'http://103.214.54.254:8080',
 }
-s = requests.Session()
 url_data = "https://www.tokocrypto.com/v1/market/trading-pairs?quoteAsset=USDT&offset=0&limit=10"
-response= s.get(url_data, proxies=proxy).json()
-print(response)
+response = cloudscraper.create_scraper()
+print(response.json())
