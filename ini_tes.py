0d92a116f9744071da0317ee1cbf7d271d35f614
Ilham Mukti <72337230+ilham-mukti@users.noreply.github.com>
2022-07-17 00:51:00 +0700

    coba lagi

--- ini_tes.py
+++ ini_tes.py
@@ -1,10 +1,11 @@
 import requests
 
 proxy = {
-    "http": 'http://185.61.152.137:8080',
+    "http": 'http://8.219.97.248:80',
+    "https": 'http://8.219.97.248:80'
 }
 url = "https://httpbin.org/ip"
-#url_data = "https://www.tokocrypto.com/v1/market/trading-pairs?quoteAsset=USDT&offset=0&limit=10"
+url_data = "https://www.tokocrypto.com/v1/market/trading-pairs?quoteAsset=USDT&offset=0&limit=10"
 #scraper = cloudscraper.create_scraper()
-response = requests.get(url, proxies=proxy)
+response = requests.get(url_data, proxies=proxy)
 print(response.json())
