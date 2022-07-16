1d2ea9d828cf3462d6a375c6c6edb08c4c2142de
Ilham Mukti <72337230+ilham-mukti@users.noreply.github.com>
2022-07-16 23:05:16 +0700

    cek lagi

--- ini_tes.py
+++ ini_tes.py
@@ -1,8 +1,7 @@
 import requests
 
 proxy = {
-    "http": 'http://138.68.238.19/:31028',
-    "http": 'http://170.39.194.16/:3128' 
+    "http": 'http://103.214.54.254:8080',
 }
 s = requests.Session()
 url_data = "https://www.tokocrypto.com/v1/market/trading-pairs?quoteAsset=USDT&offset=0&limit=10"
